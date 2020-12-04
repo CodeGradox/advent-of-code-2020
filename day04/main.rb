4293af71fa5cb81e9d7769a1eacea539596f2629
Magnus Åsrud <magnusasrud92@gmail.com>
2020-12-04 19:23:20 +0100

    Fix borked solution.

--- day04/main.rb
+++ day04/main.rb
@@ -5,14 +5,14 @@ fields = %w(byr iyr eyr hgt hcl ecl pid)
 
 puts "Part 1: #{ input.map { |l| l.split(/:#?\w+\s?/) }.count { |a| (fields - a).empty? } }"
 
-part2 = input.count do |line|
-    line.match?(/\bhcl:#[0-9a-f]{6}\b/) &&
-    line.match?(/\bpid:\d{9}\b/) &&
-    line.match?(/\becl:(amb|blu|brn|gry|grn|hzl|oth)\b/) &&
-    line.match(/\bbyr:(\d{4})\b/)&.yield_self { |m| m[1].to_i.between?(1920, 2002) } &&
-    line.match(/\biyr:(\d{4})\b/)&.yield_self { |m| m[1].to_i.between?(2010, 2020) } &&
-    line.match(/\beyr:(\d{4})\b/)&.yield_self { |m| m[1].to_i.between?(2020, 2030) } &&
-    line.match(/\bhgt:(\d{2,3})(cm|in)\b/)&.yield_self do |m|
+part2 = input.sum do |line|
+    line.match?(/hcl:#[0-9a-f]{6}\b/) &&
+    line.match?(/pid:\d{9}\b/) &&
+    line.match?(/ecl:(amb|blu|brn|gry|grn|hzl|oth)\b/) &&
+    line.match(/byr:(\d{4})\b/)&.yield_self { |m| m[1].to_i.between?(1920, 2002) } &&
+    line.match(/iyr:(\d{4})\b/)&.yield_self { |m| m[1].to_i.between?(2010, 2020) } &&
+    line.match(/eyr:(\d{4})\b/)&.yield_self { |m| m[1].to_i.between?(2020, 2030) } &&
+    line.match(/hgt:(\d{2,3})(cm|in)\b/)&.yield_self do |m|
       (m[2] == "cm" && m[1].to_i.between?(150, 193)) || (m[2] == "in" && m[1].to_i.between?(59, 76))
     end ? 1 : 0
 end
