a6748fea0f9652f2460865a006557471179e45ea
Magnus Åsrud <magnusasrud92@gmail.com>
2020-12-04 19:39:15 +0100

    Only use regex.

--- day04/main.rb
+++ day04/main.rb
@@ -9,12 +9,10 @@ part2 = input.sum do |line|
     line.match?(/hcl:#[0-9a-f]{6}\b/) &&
     line.match?(/pid:\d{9}\b/) &&
     line.match?(/ecl:(amb|blu|brn|gry|grn|hzl|oth)\b/) &&
-    line.match(/byr:(\d{4})\b/)&.yield_self { |m| m[1].to_i.between?(1920, 2002) } &&
-    line.match(/iyr:(\d{4})\b/)&.yield_self { |m| m[1].to_i.between?(2010, 2020) } &&
-    line.match(/eyr:(\d{4})\b/)&.yield_self { |m| m[1].to_i.between?(2020, 2030) } &&
-    line.match(/hgt:(\d{2,3})(cm|in)\b/)&.yield_self do |m|
-      (m[2] == "cm" && m[1].to_i.between?(150, 193)) || (m[2] == "in" && m[1].to_i.between?(59, 76))
-    end ? 1 : 0
+    line.match?(/byr:(19[2-9]\d)|(200[012])\b/) &&
+    line.match?(/iyr:((201\d)|(2020))\b/) &&
+    line.match?(/eyr:((202\d)|(2030))\b/) &&
+    line.match?(/hgt:(((1[5-8]\d|19[0-3])cm)|((59|6\d|7[0-6])in))\b/) ? 1 : 0
 end
 
 puts "Part 2: #{ part2 }"
